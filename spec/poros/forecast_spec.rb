5461a6281a93c3984aa4ec26689f086742f1ec71
Clay Ash <claycourtneyash@gmail.com>
2022-08-06 15:52:33 -0600

    Add hour poro spec

--- spec/poros/forecast_spec.rb
+++ spec/poros/forecast_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe Forecast do 
   it 'creates poro for forecast', :vcr do 
     forecast = ForecastFacade.get_forecast(39.736406, -104.89527)
+
+    expect(forecast.sunrise).to eq("08/06/2022 12:03:17 +0000")
     expect(forecast.sunset).to eq("08/07/2022 02:07:39 +0000")
+    expect(forecast.temperature).to eq(64.2)
+    expect(forecast.icon).to eq("http://openweathermap.org/img/wn/11d@2x.png")
   end
 end
---
+++ spec/poros/hour_forecast_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe HourForecast do 
+  it 'creates poro for hourly forecast', :vcr do
+     
+
+  end
+end

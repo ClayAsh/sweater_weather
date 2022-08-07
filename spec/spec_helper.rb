88f40d847953ee965b8af45324a38076e9be7290
Clay Ash <claycourtneyash@gmail.com>
2022-08-06 18:35:29 -0600

    More simplecov fun

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ SimpleCov.start 'rails' do
   add_filter '/app/jobs/'
   add_filter '/app/mailers/'
   add_filter 'app/controllers/application_controller.rb'
+  add_filter '/app/models/application_record.rb'
 end 
 
   WebMock.disable_net_connect!

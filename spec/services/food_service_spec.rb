7ffd30f06136b331c076f6cb1fdc04bed0102567
Clay Ash <claycourtneyash@gmail.com>
2022-08-08 09:53:50 -0600

    Add expectations to food service spec

--- spec/services/food_service_spec.rb
+++ spec/services/food_service_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 RSpec.describe FoodService do 
   it 'establishes connection to return restaurant data', :vcr do 
     location = "Pueblo, CO"
-    categories = "Chinese Restaurant"
+    term = "Chinese Restaurant"
 
-    result = FoodService.get_food_info(location, categories)
+    result = FoodService.get_food_info(location, term)
 
     expect(result).to be_a(Hash)
+    expect(result[:businesses][0]).to include(:id)
+    expect(result[:businesses][0]).to include(:name)
+    expect(result[:businesses][0]).to include(:location)
   end
 end
